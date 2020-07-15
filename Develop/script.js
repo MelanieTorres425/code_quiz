8f5ab5ef56336681c09b14038e3a29153ff268cf
Bugs <melanietorres425@live.com>
2020-07-15 01:21:10 -0400

    updated almost everything

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,14 @@
 let startBtn = document.querySelector("#startBtn");
-let answerBtn = document.querySelector("#answerBtn");
+let answerBtn1 = document.querySelector("#answerBtn1");
+let answerBtn2 = document.querySelector("#answerBtn2");
+let answerBtn3 = document.querySelector("#answerBtn3");
+let answerBtn4 = document.querySelector("#answerBtn4");
+let ansBtn = document.querySelector(".ansBtn");
 let isCorrect = document.querySelector("#correct_wrong");
 let displayForm = document.querySelector("#questionDiv");
 let timerEl = document.querySelector("#countdown");
+let headerEl = document.querySelector("#header");
+let startPage = document.querySelector("#startPage");
 
 const quizQA = [
   {
@@ -43,6 +49,8 @@ const quizQA = [
 // WHEN I click the start button
 function startGame() {
   timeStart(event);
+  ansBtn.classList.remove("hide");
+  updateDisplay();
 }
 
 // THEN a timer starts and I am presented with a question
@@ -61,7 +69,19 @@ function timeStart(event) {
   }, 1000);
 }
 
+let questionCounter = 0;
+
+function updateDisplay() {
+  console.log(quizQA[questionCounter].question);
+  headerEl.textContent = quizQA[questionCounter].question;
+  answerBtn1.textContent = quizQA[questionCounter].choices[0];
+  answerBtn2.textContent = quizQA[questionCounter].choices[1];
+  answerBtn3.textContent = quizQA[questionCounter].choices[2];
+  answerBtn4.textContent = quizQA[questionCounter].choices[3];
+}
+
 startBtn.addEventListener("click", function () {
   startBtn.classList.add("hide");
+  startPage.classList.add("hide");
   startGame();
 });
